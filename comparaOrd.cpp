d6cfca9957649034bf8399cda460c232007fdb76
Yair292 <dluquep@unjbg.edu.pe>
2024-10-04 23:02:36 -0500

    Actualizar errores de quick sort en cantidades grandes, cambiando el algoritmo

--- comparaOrd.cpp
+++ comparaOrd.cpp
@@ -7,17 +7,22 @@
 #include <chrono>
 
 using namespace std;
+
+//mis prototipos...
 void metodoBurbuja(int [],int );
-void quickSort(int [],int );
-void reduce(int [],int ,int );
+void quickSort(int[], int, int);
+int partition(int [], int, int );
+void tiempoBurbuja(int a[], int n);
+void tiempoQuickSort(int [], int, int);
+
 
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
 	int n,op,cantidad;
 	
-	
 	cout << "Digite la cantidad:\n"; cin >> n;
 	int numeros[n];
+	int copia[n];
 	cout << "Digite el limite del arreglo (¿desde 0 hasta?):\n"; cin >> cantidad;
 	srand(time(0));
 	for(int i =0;i<n;i++){
@@ -28,8 +33,13 @@ int main(){
 	for (int i =0;i<n;i++){
 		cout << numeros[i]<<" ";
 	}
-		
 	do{
+		//copia de seguridad.
+		for(int i =0;i<n;i++){
+			copia[i] = numeros[i];
+		}
+		//fin de copia de seguridad
+			
 		cout << "\n\n\nBienvenido a la comparacion de dos metodos de ordenamiento"
 		<<"\n1. Método de Burbuja."
 		<<"\n2. Método de QuickSort."
@@ -37,77 +47,84 @@ int main(){
 		cin >>op;
 		switch (op){
 			case 1:
-				metodoBurbuja(numeros,n-1);
+				tiempoBurbuja(copia,n-1);	
 				break;
+			
 			case 2:
-				quickSort(numeros,n-1);
+				tiempoQuickSort(copia, 0, n-1);
 				break;
-		cout << "Arreglo ordenado.\n";
-		for (int i =0;i<n;i++){
-			cout << numeros[i]<<" ";
-		}	
 		}
 	}while(op != 0);
-		
-		
+				
 	return 0;
 }
 
 //primero a comprarar
-void metodoBurbuja(int a[],int  n){
-	auto inicio = chrono::high_resolution_clock::now();
+void metodoBurbuja(int a[], int n) {
 	int aux;
-	for(int i=0;i<n;i++){
-		for(int j =0;j<n;j++){
-			if (a[j]>a[j+1]){
+	for (int i =0; i<n; i++) {
+		for (int j =0; j<n; j++) {
+			if (a[j] > a[j+1]) {
 				aux = a[j];
-				a[j]=a[j+1];
+				a[j] = a[j+1];
 				a[j+1] = aux;
 			}
 		}
 	}
+}
+
+void tiempoBurbuja(int a[], int n) {
+	auto inicio = chrono::high_resolution_clock::now();	
+	metodoBurbuja(a, n);	
 	auto fin = chrono::high_resolution_clock::now();
-	chrono::duration<double>duracion = fin-inicio;
-	cout << "\nEl tiempo de ejecucion del algoritmo fue de : "<<duracion.count()<<" segundos."<<endl;
+	chrono::duration<double> duracion = fin - inicio;
+	cout << "\nArreglo ordenado.\n";
+	for (int i =0;i<n;i++){
+		cout << a [i]<<" ";
+	}
+	cout << "\nEl tiempo de ejecucion del algoritmo Burbuja fue de: " <<duracion.count()<<" segundos."<<endl;
 }
+
+
+
+
 //segundo a comparar
-void quickSort(int A[],int n){
-	reduce(A, 0 ,n);
+void quickSort(int A[], int inicio, int final) {
+	if (inicio < final) {
+		int pivotIndex = partition(A, inicio, final);
+		quickSort(A, inicio, pivotIndex - 1); // Lado izquierdo del pivote
+		quickSort(A, pivotIndex+1, final);  // Lado derecho del pivote
+	}
 }
-void reduce(int A[],int inicio,int final){
-	int cen, pos,aux;
-	int izq = inicio;
-	int der = final;
-	pos = izq;
-	cen = 1;
-	while(cen == 1){
-		cen = 0;
-		while((A[pos] <= A[der]) && (pos != der)){
-			der--;
-		}
-		if (pos != der){
-			aux = A[pos];
-			A[pos]= A[der];
-			A[der]=aux;
-			pos = der;
-			//recorrido de izquierda a derecha
-			while((A[pos] >= A[izq]) && (pos != izq)){
-				izq++;
-			}
-			if (pos != izq){
-				aux = A[pos];
-				A[pos] = A[izq];
-				A[izq] = aux;
-				pos = izq;
-				cen =1;
-			}
-		}
-		if (pos-1 > inicio){
-			reduce(A,inicio,pos-1);
-		}
-		if (pos+1<final){
-			reduce(A, pos+1, final);
+
+int partition(int A[], int inicio, int final) {
+	int pivot = A[final];
+	int i = inicio-1;
+	int aux;
+
+	for (int j = inicio; j < final; j++) {
+		if (A[j] <= pivot) {
+			i++;
+			aux = A[i];
+			A[i] = A[j];
+			A[j] = aux;
 		}
 	}
+	aux = A[i+1];
+	A[i+1] = A[final];
+	A[final] = aux;
 
+	return i+1;
+}
+
+void tiempoQuickSort(int A[], int inicio, int final) {
+    auto empezar = chrono::high_resolution_clock::now();
+    quickSort(A, inicio, final);   
+    auto terminar = chrono::high_resolution_clock::now();
+    chrono::duration<double> duracion = terminar - empezar;  
+	cout << "\nArreglo ordenado.\n";
+	for (int i =0;i<final;i++){
+		cout << A[i]<<" ";
+	} 
+    cout << "\nEl tiempo de ejecucion del algoritmo QuickSort fue de: " << duracion.count() <<" segundos."<<endl;
 }
